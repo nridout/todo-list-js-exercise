00320498a3718c637dc2c72bb49ef6642e79cdf2
Nicole Ridout <nicole.ridout@gmail.com>
2019-03-05 15:45:09 +0000

    completed todo list code

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+// Task Object contains properties
+// calling the newTask while passing the title and description of the tasks
+// will create a new task obejct with those values attached to the properties
+// completed is set to false as the default state
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
 
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+    // prints out the provided task's details
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    // marks the provided task as completed
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
+// DRIVER CODE
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+const task1 = newTask("Clean Cat Litter", "Take all the shit out of the litter box");
+const task2 = newTask("Do Laundry", "Annoying");
+// An array of the task objects, aka the "todo list"
+const tasks =[task1, task2];
 
-// DRIVER CODE BELOW
+// task1.logState(task1);
+// task1.markCompleted(task1);
+// task1.logState(task1);
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
-
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+console.log(task1.title);
+task1.markCompleted();
+console.log(task1.complete);
